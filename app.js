012b70c52757c326bb13275f6d47586cb3ee0fd9
Isi Tony-Eigbokhan <itonyeig@gmail.com>
2021-03-02 16:23:59 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname +'/public'));
+let items = ['buy food','buy drink'];
+
+app.get('/', function (req, res){
+    let today = new Date();
+    let currentDay = today.getDay();
+    let day = "";
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+    console.log(today.toLocaleDateString("en-US", options));
+    day = today.toLocaleDateString("en-US", options);
+
+
+    app.post('/', function(req, res){
+        let item = req.body.newItem;
+        items.push(item);
+        console.log(items);
+        res.redirect('/');
+    });
+    res.render('list', {
+        kindOfDay: day,
+        newListItems: items
+    });
+});
+
+
+app.listen(3000, function(){
+    console.log('Server is running on port 3000');
+})
